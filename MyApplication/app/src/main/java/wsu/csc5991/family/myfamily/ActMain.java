35ce49ff7940e37646a640ffb40c9fe6eb293b02
Vimal, Sasidharan (DS) <vimal.sasidharan@cdk.com>
2016-06-18 18:34:48 -0400

    Adding a layout for Toast

--- MyApplication/app/src/main/java/wsu/csc5991/family/myfamily/ActMain.java
+++ MyApplication/app/src/main/java/wsu/csc5991/family/myfamily/ActMain.java
@@ -2,6 +2,9 @@ package wsu.csc5991.family.myfamily;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class ActMain extends AppCompatActivity {
 
@@ -9,5 +12,83 @@ public class ActMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lay_main);
+        //Toast.makeText(getApplicationContext(), "Message", Toast.LENGTH_LONG).show();
+
+        View view;
+        Toast toast;
+        view = getLayoutInflater().inflate(R.layout.lay_notification, null);
+        toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setView(view);
+        toast.show();
+
     }
+
+    //----------------------------------------------------------------
+    // Variables
+    //----------------------------------------------------------------
+
+
+
+    //----------------------------------------------------------------
+    // onStart
+    //----------------------------------------------------------------
+    @Override
+    protected void onStart()
+    {
+        System.out.println("### DEBUG ### onStart");
+        super.onStart();
+    }
+
+    //----------------------------------------------------------------
+    // onResume
+    //----------------------------------------------------------------
+    @Override
+    protected void onResume()
+    {
+        System.out.println("### DEBUG ### onResume");
+        super.onResume();
+    }
+
+    //----------------------------------------------------------------
+    // onPause
+    //----------------------------------------------------------------
+    @Override
+    protected void onPause()
+    {
+        System.out.println("### DEBUG ### onPause");
+        super.onPause();
+    }
+
+    //----------------------------------------------------------------
+    // onStop
+    //----------------------------------------------------------------
+    @Override
+    protected void onStop()
+    {
+        System.out.println("### DEBUG ### onStop");
+        super.onStop();
+    }
+
+    //----------------------------------------------------------------
+    // onRestart
+    //----------------------------------------------------------------
+    @Override
+    protected void onRestart()
+    {
+        System.out.println("### DEBUG ### onRestart ");
+        super.onRestart();
+    }
+
+    //----------------------------------------------------------------
+    // onDestroy
+    //----------------------------------------------------------------
+    @Override
+    protected void onDestroy()
+    {
+        System.out.println("### DEBUG ### onDestroy ");
+        super.onDestroy();
+    }
+
 }
