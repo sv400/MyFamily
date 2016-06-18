285e7daf117f37c03f7691b407e29776c3a401cf
Vimal, Sasidharan (DS) <vimal.sasidharan@cdk.com>
2016-06-18 17:15:29 -0400

    Initial Commit

---
+++ MyApplication/app/src/main/java/wsu/csc5991/family/myfamily/ActMain.java
@@ -0,0 +1,13 @@
+package wsu.csc5991.family.myfamily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_main);
+    }
+}
